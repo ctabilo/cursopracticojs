d6d23f7562f168ab97faaeb68955f027564c7f07
ctabilo <cristiantabilol@gmail.com>
2022-01-25 22:26:05 -0300

    25012022

--- figuras.js
+++ figuras.js
@@ -1,1 +1,20 @@
-console.log("hola mundo")
+console.group("Cuadrados");
+    const ladocuadrado = 5;
+    console.log("Los lados del cuadrado miden: " + ladocuadrado + "cm");
+
+    const perimetrocuadrado = ladocuadrado * 4;
+    console.log("El perímetro del cuadrado mide: " + perimetrocuadrado + "cm");
+
+    const areacuadrado = ladocuadrado * ladocuadrado;
+    console.log("El área del cuadrado mide: " + areacuadrado + "cm^2");
+console.groupEnd();
+
+//Código del triangulo
+console.group("Triángulos");
+    const ladotriangulo1 = 6
+    const ladotriangulo2 = 6
+    const basetriangulo = 4
+    console.log("Los lados del triángulo son: " + ladotriangulo1 + "cm, " + ladotriangulo3 + "cm, " + basetriangulo + "cm")
+
+    
+console.groupEnd();
