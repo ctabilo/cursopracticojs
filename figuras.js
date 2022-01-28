eb1587424a696a0018e5d7f9d1d2be46a36ce826
ctabilo <cristiantabilol@gmail.com>
2022-01-28 08:54:36 -0300

    antes de las funciones

--- figuras.js
+++ figuras.js
@@ -14,7 +14,36 @@ console.group("Triángulos");
     const ladotriangulo1 = 6
     const ladotriangulo2 = 6
     const basetriangulo = 4
-    console.log("Los lados del triángulo son: " + ladotriangulo1 + "cm, " + ladotriangulo3 + "cm, " + basetriangulo + "cm")
+    console.log("Los lados del triángulo son: " + ladotriangulo1 + "cm, " + ladotriangulo2 + "cm, " + basetriangulo + "cm")
 
+    const alturatriangulo = 5.5;
+    console.log ("La altura del triángulo es: " + alturatriangulo);
+
+    const perimetrotriangulo = ladotriangulo1 + ladotriangulo2 + basetriangulo;
+    console.log("El perímetro del triángulo es: " + perimetrotriangulo);
+
+    const areatriangulo = (basetriangulo * alturatriangulo) / 2;
+    console.log("El área del triángulo es: " + areatriangulo);
     
+console.groupEnd();
+
+//Código del círculo
+console.group("Círculos");
+    //Radio
+    const radio = 4;
+    console.log("el radio del circulo es: " + radio);
+
+    //Diametro
+    const diametro = radio * 2;
+    console.log("el diámetro del circulo es: " + diametro);
+    //PI
+    const PI = Math.PI;
+    console.log("PI es: " + PI);
+    //Circunferencia
+    const perimetrocirculo = diametro * PI;
+    console.log("el perimetro del circulo es: " + perimetrocirculo);
+    //Área
+    const areacirculo = (radio * radio) * PI;
+    console.log("el área del circulo es: " + areacirculo);
+
 console.groupEnd();
